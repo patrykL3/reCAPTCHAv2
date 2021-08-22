18c484cb5296752d88f09c2b1442763f9953ee1a
Patryk Łubik <patryk.lubik@wp.pl>
2021-08-22 11:49:15 +0200

    skeleton

---
+++ src/test/java/pl/patryklubik/reCAPTCHAv2/ReCaptchAv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.patryklubik.reCAPTCHAv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReCaptchAv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
